cd06f002d67fadeb5df40eddf564375fe4ae87c7
Shiv Rai <sshaktirai@gmail.com>
2024-10-28 16:14:03 +0530

    Part11 done

---
+++ lab-bulma-components/src/App.js
@@ -0,0 +1,15 @@
+import  "bulma/css/bulma.css";
+import './App.css';
+import "./components/Navbar"
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+
